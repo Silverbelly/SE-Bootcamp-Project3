747b0b416fe001e22337e52e6136f612921cca6d
Silverbelly <jonnoel@outlook.com>
2025-01-27 17:47:21 -0600

    remove css for layout component

--- website/src/pages/Layout.jsx
+++ website/src/pages/Layout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../page-styles/Layout.css';
 
 const Layout = () => {
   return (
     <>
       <Navbar expand="md" className="bg-info pe-3" fixed="top">
-          <Navbar.Brand href="/" className="ms-5 p-0"><img alt="logo" src='./logoCL.png' width="60" /></Navbar.Brand>
+          <Navbar.Brand href="/" className="ms-5 p-0"><img alt="logo" src='/logoCL.png' width="60" /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-5 me-auto">
